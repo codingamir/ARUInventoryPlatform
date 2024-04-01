a6258c6b09bbab1e76966a6a836f75d2e7186f7d
jdjoshua <159008471+jdjoshua@users.noreply.github.com>
2024-04-01 12:44:29 +0100

    Add Product Capabilities in productDetail View and passed it to template

--- pages/views/product.py
+++ pages/views/product.py
@@ -1,11 +1,13 @@
 
 from django.shortcuts import render
-from pages.models import Product
-from pages.forms import ProductForm
+from pages.models import Product, ProductCapability
+from pages.forms import ProductForm, ProductCapabilityForm
 
 def productDetail(request):
     product_id = request.GET.get("id")
     product = Product.objects.get(pk=product_id)
-
+    productCapability = ProductCapability.objects.filter(Product_id = product_id).first()
+    
     productForm = ProductForm(instance=product)
-    return render(request, "pages/products/productDetail.html", {"product": productForm} )
+    productCapabilityForm = ProductCapabilityForm(instance=productCapability)
+    return render(request, "pages/products/productDetail.html", {"product": productForm, "productCapability": productCapabilityForm} )
