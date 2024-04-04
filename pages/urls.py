c0eb06b2b85c1c70b283cf37db8802a94577ef6c
Amir Ejaz <muhammadamirejaz@gmail.com>
2024-04-04 14:10:32 +0100

    updated __init__.py in views folder
    Signed-off-by: Amir Ejaz <muhammadamirejaz@gmail.com>

--- pages/urls.py
+++ pages/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path, include
-from views import home, registeration, product, vendor
+from pages import views
 
 urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
-    path("", home.home, name='home'),
-    path("home", home.home, name='home'),
-    path("vendors", home.home, name='home'),
-    path("login", registeration.login_view, name='login'),
-    path("logout", registeration.custom_logout, name='logout'),
-    path("validate", registeration.validateAndRedirect, name='validate'),
-    path("dashboard", home.home, name='home'),
-    path("products", product.products, name='products'),
-    path("productDetail", product.productDetail, name='products'),
-    path("vendor", vendor.vendor, name='vendor'),
-    path("updateVendor", vendor.updateVendor, name='vendor'),
+    path("", views.home, name='home'),
+    path("home", views.home, name='home'),
+    path("vendors", views.home, name='home'),
+    path("login", views.login_view, name='login'),
+    path("logout", views.custom_logout, name='logout'),
+    path("validate", views.validateAndRedirect, name='validate'),
+    path("dashboard", views.home, name='home'),
+    path("products", views.products, name='products'),
+    path("productDetail", views.productDetail, name='products'),
+    path("vendor", views.vendor, name='vendor'),
+    path("updateVendor", views.updateVendor, name='vendor'),
 
-    path("contactUs", registeration.contactUs, name='contactUs'),]
+    path("contactUs", views.contactUs, name='contactUs'),
+]
---
+++ pages/views/__init__.py
@@ -0,0 +1,4 @@
+from pages.views.home import *
+from pages.views.product import *
+from pages.views.registeration import *
+from pages.views.vendor import *
